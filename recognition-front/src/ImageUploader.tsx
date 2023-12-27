3ff29d3354d654bdf435fca11912f45b21fa8073
boheng <c451735634@163.com>
2023-12-27 23:58:42 +0900

    update Image Uploader 1. now you can see the picture you uploaded on the page 2. fix the bug occured when you click "select file" but select nothing and just cancel 3. now the image will not be uploaded until you click "Upload"

--- recognition-front/src/ImageUploader.tsx
+++ recognition-front/src/ImageUploader.tsx
@@ -1,44 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import axios from 'axios';
 
 const ImageUploader: React.FC = () => {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [imagePreviewUrl, setImagePreviewUrl] = useState<string | null>(null);
     const [uploadResult, setUploadResult] = useState<{ class: string; probability: number } | null>(null);
+
+    const previewImage = (file: File) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setImagePreviewUrl(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+    };
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
-            setSelectedFile(event.target.files[0]);
+            if (event.target.files && event.target.files.length > 0) {
+                const file = event.target.files[0];
+                setSelectedFile(file);
+                previewImage(file);
+                setUploadResult(null);
+            }
         }
     };
 
-    const handleSubmit = async () => {
-        if (selectedFile) {
-            const formData = new FormData();
-            formData.append('file', selectedFile);
+    const uploadImage = async (file: File) => {
+        const formData = new FormData();
+        formData.append('file', file);
 
-            try {
-                const response = await axios.post('http://localhost:8000/uploadfile/', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                });
-                setUploadResult(response.data);
-            } catch (error) {
-                console.error('Error uploading file:', error);
-            }
+        try {
+            const response = await axios.post('http://localhost:8000/uploadfile/', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            setUploadResult(response.data);
+        } catch (error) {
+            console.error('Error uploading file:', error);
         }
     };
 
     return (
         <div>
             <input type="file" onChange={handleFileChange} />
-            <button onClick={handleSubmit}>Upload</button>
-            {uploadResult && (
-                <div>
-                    <p>Class: {uploadResult.class}</p>
-                    <p>Probability: {uploadResult.probability.toFixed(2)}</p>
+            <button onClick={() => selectedFile && uploadImage(selectedFile)}>Upload</button>
+            {imagePreviewUrl && (
+                <div style={{ marginTop: '10px' }}>
+                    <img
+                        src={imagePreviewUrl}
+                        alt="Preview"
+                        style={{ width: 'auto', height: '200px' }}
+                    />
                 </div>
             )}
+            <div>
+                {uploadResult && (
+                    <div>
+                        <p>Class: {uploadResult.class}</p>
+                        <p>Probability: {uploadResult.probability.toFixed(2)}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
