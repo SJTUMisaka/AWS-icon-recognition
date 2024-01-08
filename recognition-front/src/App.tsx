a7734aa76d16ba3749a836a5b90d8ed307c6800d
boheng <c451735634@163.com>
2024-01-08 21:27:26 +0900

    Start using tailwind css

--- recognition-front/src/App.tsx
+++ recognition-front/src/App.tsx
@@ -3,8 +3,12 @@ import ImageUploader from './ImageUploader';
 
 function App() {
   return (
-    <div className="App">
-      <h1>AWS ICON RECOGNITION</h1>
+    <div>
+      <header className="bg-blue-600 p-4 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-3xl font-bold">AWS Icon Recognition</h1>
+        </div>
+      </header>
       <ImageUploader />
     </div>
   );
